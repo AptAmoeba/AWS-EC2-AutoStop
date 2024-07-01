563bd76b187111c34d781f2eeb8232f0ed520fd4
Austin <adalsing1102@gmail.com>
2024-07-01 18:56:07 +0000

    Update AnnotatedPowershell.ps1

--- AnnotatedPowershell.ps1
+++ AnnotatedPowershell.ps1
@@ -2,16 +2,16 @@
 
 #-Command "
 $U = 'Administrator';
-$Fname = $Fname = $env:COMPUTERNAME + "\" + $U;
+$Fname = $env:COMPUTERNAME + "\" + $U;
 $event24 = (Get-WinEvent -FilterHashtable @{LogName='Microsoft-Windows-TerminalServices-LocalSessionManager/Operational'; ID=24} -MaxEvents 1).message
-if ($event24 -match $Fname) {
+if ($event24 -contains $Fname) {
 	#Wait through grace period
 	Start-Sleep -Seconds 240
 	
 	#Checking for reconnect within grace period
 	$event25 = Get-WinEvent -FilterHashtable @{LogName='Microsoft-Windows-TerminalServices-LocalSessionManager/Operational'; ID=25} -MaxEvents 1
 	
-	if ($event25.Properties[0].Value -match $Fname -and $event25.TimeCreated -gt (Get-Date).AddMinutes(-4)) {
+	if ($event25.Properties[0].Value -eq $Fname -and $event25.TimeCreated -gt (Get-Date).AddMinutes(-4)) {
 		Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Reconnected; shutdown aborted', 'EC2-Status')
 		#Debug: Write-Output '$U reconnect within grace period detected. Aborting.'
 	} else {
