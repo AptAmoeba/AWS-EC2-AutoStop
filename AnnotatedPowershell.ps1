b5c923f01e0fc30814744d1ca0cbc07ac21c7d6f
Austin <adalsing1102@gmail.com>
2024-07-01 19:32:59 +0000

    Update AnnotatedPowershell.ps1
    I'm doing a great job of breaking things today, eh?

--- AnnotatedPowershell.ps1
+++ AnnotatedPowershell.ps1
@@ -3,13 +3,13 @@
 #-Command "
 $U = 'Administrator';
 $Fname = $env:COMPUTERNAME + "\" + $U;
-$event24 = (Get-WinEvent -FilterHashtable @{LogName='Microsoft-Windows-TerminalServices-LocalSessionManager/Operational'; ID=24} -MaxEvents 1).message
-if ($event24 -contains $Fname) {
+$event24 = (Get-WinEvent -FilterHashtable @{LogName='Microsoft-Windows-TerminalServices-LocalSessionManager/Operational'; ID=24} -MaxEvents 1)
+if ($event24.Properties[0].Value -eq $Fname) {
 	#Wait through grace period
 	Start-Sleep -Seconds 240
 	
 	#Checking for reconnect within grace period
-	$event25 = Get-WinEvent -FilterHashtable @{LogName='Microsoft-Windows-TerminalServices-LocalSessionManager/Operational'; ID=25} -MaxEvents 1
+	$event25 = (Get-WinEvent -FilterHashtable @{LogName='Microsoft-Windows-TerminalServices-LocalSessionManager/Operational'; ID=25} -MaxEvents 1)
 	
 	if ($event25.Properties[0].Value -eq $Fname -and $event25.TimeCreated -gt (Get-Date).AddMinutes(-4)) {
 		Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Reconnected; shutdown aborted', 'EC2-Status')
